2711aff950980f68c30e4e3734d9febac75f2818
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-10-15 01:57:52 +0530

    Create Form Biggest Number.cpp

---
+++ Assignment3/Form Biggest Number.cpp
@@ -0,0 +1,70 @@
+// You are provided an array of numbers. You need to arrange them in a way that yields the largest value.
+
+// Input Format
+// First line contains integer t which is number of test case.
+// For each test case, you are given a single integer n in the first line which is the size of array A[] and next line contains n space separated integers denoting the elements of the array A .
+
+// Constraints
+// 1<=t<=100
+
+// 1<=m<=100
+
+// 1<=A[i]<=10^5
+
+// Output Format
+// Print the largest value.
+
+// Sample Input
+// 1
+// 4
+// 54 546 548 60
+// Sample Output
+// 6054854654
+// Explanation
+// Upon rearranging the elements of the array , 6054854654 is the largest possible number that can be generated.
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int myCompare(string X, string Y)
+{
+    // first append Y at the end of X
+    string XY = X.append(Y);
+
+    // then append X at the end of Y
+    string YX = Y.append(X);
+
+    // Now see which of the two formed numbers is greater
+    return XY.compare(YX) > 0 ? 1: 0;
+}
+int main(int argc, char* argv[])
+{
+    int t;
+    int n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        vector<string> arr;
+        for(int i=0;i<n;i++) {
+            string temp;
+            cin>>temp;
+            arr.push_back(temp);
+        }
+        sort(arr.begin(), arr.end(), myCompare);
+        for(int i=0;i<n;i++) {
+            cout<<arr[i];
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
