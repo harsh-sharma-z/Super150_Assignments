2fe548f49c7b64e15c24a4b8b4351b8ca4399446
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-10-15 00:10:19 +0530

    Update Print Primes.java

--- Assignment1/Print Primes.java
+++ Assignment1/Print Primes.java
@@ -1,3 +1,22 @@
+// Take N as input. Print all prime numbers from 2 to N.
+
+// Input Format
+// Constraints
+// 1 <= N <= 1000
+
+// Output Format
+// Sample Input
+// 10
+// Sample Output
+// 2
+// 3
+// 5
+// 7
+// Explanation
+// Each output should be on separate line. A prime number is that number which is divisible by one or itself.
+	
+
+
 import java.util.*;
 public class Main {
 	public static void prime(int n)
