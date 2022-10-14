c46a1e282bc47ae97eb002657b62195d1674dcfe
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-10-15 01:10:17 +0530

    Create Find Square Root.cpp

---
+++ Assignment2/Find Square Root.cpp
@@ -0,0 +1,34 @@
+// Implement squareroot(x).
+
+// Compute and return the square root of x.
+
+// If x is not a perfect square, return floor(sqrt(x)).
+// Note: DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY
+
+// Input Format
+// An integer A .
+
+// Constraints
+// 0 <= A <10000000000
+
+// Output Format
+// Squareroot of A.
+
+// Sample Input
+// 25
+// Sample Output
+// 5
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int squareRoot(int A) {
+}
+int main () {
+        int A;
+        cin >> A;
+        cout << squareRoot(A);
+	return 0;
+}
