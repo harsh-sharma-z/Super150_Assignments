47a7c3011432c6f21b7051b93519ba987b66554d
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-12-08 13:26:30 +0530

    Create Form minimum number from given Sequence.java

---
+++ Assignment5/Form minimum number from given Sequence.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class Main {
+    static void PrintMinNumberForPattern(String arr)
+    {
+        int curr_max = 0;
+        int last_entry = 0;
+        int j;
+        for (int i = 0; i < arr.length(); i++)
+        {
+            int noOfNextD = 0;
+            switch (arr.charAt(i))
+            {
+                case 'I':
+                    j = i + 1;
+                    while (j < arr.length() && arr.charAt(j) == 'D')
+                    {
+                        noOfNextD++;
+                        j++;
+                    }
+
+                    if (i == 0)
+                    {
+                        curr_max = noOfNextD + 2;
+                        System.out.print("" + ++last_entry);
+                        System.out.print("" + curr_max);
+                        last_entry = curr_max;
+                    }
+                    else
+                    {
+                        curr_max = curr_max + noOfNextD + 1;
+                        last_entry = curr_max;
+                        System.out.print("" + last_entry);
+                    }
+                    for (int k = 0; k < noOfNextD; k++)
+                    {
+                        System.out.print("" + --last_entry);
+                        i++;
+                    }
+                    break;
+                case 'D':
+                    if (i == 0)
+                    {
+                        j = i + 1;
+                        while (j < arr.length()&&arr.charAt(j) == 'D')
+                        {
+                            noOfNextD++;
+                            j++;
+                        }
+                        curr_max = noOfNextD + 2;
+                        System.out.print("" + curr_max + "" + (curr_max - 1));
+
+                        last_entry = curr_max - 1;
+                    }
+                    else
+                    {
+                        System.out.print("" + (last_entry - 1));
+                        last_entry--;
+                    }
+                    break;
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            String s=sc.next();
+            PrintMinNumberForPattern(s);
+        }
+
+    }
+}
