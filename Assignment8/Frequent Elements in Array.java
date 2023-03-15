9d7d077f4a7021a72ea9fb8e97be6d2155ee7e71
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-03-15 10:26:31 +0530

    Create Frequent Elements in Array.java

---
+++ Assignment8/Frequent Elements in Array.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class Main {
+    public static int[] topK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(Integer i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        ArrayList< Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+
+        list.sort((a,b) -> Objects.equals(a.getValue(), b.getValue())? b.getKey()-a.getKey():b.getValue()-a.getValue());
+
+        int[] ans= new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=list.get(i).getKey();
+        }
+
+        //   System.out.println(list);
+        return ans;
+    }
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int arr[]=new  int[n];
+            for(int i=0;i<n;i++)
+                arr[i]=sc.nextInt();
+            int ans[]=topK(arr,k);
+			Arrays.sort(ans);
+            for(int i:ans)
+                System.out.print(i+" ");
+
+
+    }
+}
