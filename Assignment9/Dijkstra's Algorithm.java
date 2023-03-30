63be17d146b3bab0ea172fa83b35f418c0303921
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-03-30 22:50:14 +0530

    Create Dijkstra's Algorithm.java

---
+++ Assignment9/Dijkstra's Algorithm.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+     static class Pair{
+
+        int distance;
+        int node;
+
+        public Pair(int distance, int node){
+            this.distance=distance;
+            this.node=node;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
+            for(int i=0;i<n;i++)
+            {
+                adj.add(new ArrayList<Pair>());
+            }
+
+            int edge=sc.nextInt();
+            while(edge-->0)
+            {
+                int u=sc.nextInt();
+                int v=sc.nextInt();
+                int d=sc.nextInt();
+                u--;
+                v--;
+                adj.get(u).add(new Pair(d,v));
+                adj.get(v).add(new Pair(d,u));
+            }
+
+            int src=sc.nextInt();
+            src--;
+
+            int dist[]=new int[n];
+            Arrays.fill(dist,1000000007);
+
+
+            dijkstra(n,adj,src,dist);
+
+            for(int i=0;i<n;i++)
+            {
+                if(i==src)
+                    continue;
+                if(dist[i]==1000000007)
+                    System.out.print("-1 ");
+                else
+                    System.out.print(dist[i]+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void dijkstra(int V, ArrayList<ArrayList<Pair>> adj, int S, int[] dist)
+    {
+        // Write your code here
+        PriorityQueue<Pair> uwu=new PriorityQueue<Pair>((x,y) -> x.distance - y.distance);
+
+//        int[] dist=new int[V];
+//        Arrays.fill(dist,1000000007);
+
+        dist[S]=0;
+
+        uwu.add(new Pair(0,S));
+
+
+        while(!uwu.isEmpty())
+        {
+            int dis=uwu.peek().distance;
+            int node=uwu.peek().node;
+
+            uwu.poll();
+
+            for(Pair i:adj.get(node))
+            {
+                int edgew=i.distance;
+                int adjNode=i.node;
+
+                if(dis+edgew< dist[adjNode])
+                {
+                    dist[adjNode]=dis+edgew;
+                    uwu.add(new Pair(dist[adjNode],adjNode));
+                }
+            }
+        }
+
+       return;
+    }
+
+}
